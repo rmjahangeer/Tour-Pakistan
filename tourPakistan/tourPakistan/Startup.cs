d55c786d3f40067df316c968d2337325d36c55a6
bitf12m015 <bitf12m015@pucit.edu.pk>
2015-10-31 22:01:11 +0500

    Initial Commit

---
+++ tourPakistan/tourPakistan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tourPakistan.Startup))]
+namespace tourPakistan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
