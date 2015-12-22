f405bd3a3eb708bc872cf7065fb9acbb96c78a70
bitf12m015 <bitf12m015@pucit.edu.pk>
2015-12-22 15:45:57 +0500

    changes in views

---
+++ tourPakistan/Models/WebModels/LocationImageWebModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.WebModels
+{
+    public class LocationImageWebModel
+    {
+        public long ImageId { get; set; }
+        public string Path { get; set; }
+        public long LocationId { get; set; }
+        public string ImageType { get; set; }
+    }
+}
---
+++ tourPakistan/Repository/Repositories/LocationImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class LocationImage
+    {
+        public long ImageId { get; set; }
+        public string Path { get; set; }
+        public long LocationId { get; set; }
+        public string ImageType { get; set; }
+    }
+}
